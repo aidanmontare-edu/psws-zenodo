1b9b1f490ee42f0dac6878c2b9edb72a3c2b912b
Aidan Montare <aidan@aidanmontare.net>
2020-08-06 02:22:34 -0400

    code formatting

--- setup.py
+++ setup.py
@@ -1,26 +1,30 @@
 # -*- coding: utf-8 -*-
 
-import setuptools
 import codecs
 import os
 
+import setuptools
+
 """Thanks to pip"""
+
 
 def read(rel_path):
     here = os.path.abspath(os.path.dirname(__file__))
     # intentionally *not* adding an encoding option to open, See:
     #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
-    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
         return fp.read()
+
 
 def get_version(rel_path):
     for line in read(rel_path).splitlines():
-        if line.startswith('__version__'):
+        if line.startswith("__version__"):
             # __version__ = "0.9"
             delim = '"' if '"' in line else "'"
             return line.split(delim)[1]
     else:
         raise RuntimeError("Unable to find version string.")
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -52,7 +56,7 @@ setuptools.setup(
         "Topic :: Communications :: Ham Radio",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Atmospheric Science"
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
     ],
-    python_requires='>=3.5',
+    python_requires=">=3.5",
 )
